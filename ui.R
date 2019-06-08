923fcff4930e676ee7c4b9a68ec75d8ce8988389
chengchengchels <421232370@qq.com>
2019-06-08 03:33:39 -0700

    Update ui.R

--- ui.R
+++ ui.R
@@ -1,21 +1,11 @@
-#
-# This is the user-interface definition of a Shiny web application. You can
-# run the application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-# 
-#    http://shiny.rstudio.com/
-#
+
 
 library(shiny)
 
-# Define UI for application that draws a histogram
 shinyUI(fluidPage(
-  
-  # Application title
+
   titlePanel("Text Analysis: MyCourses Feedbacks"),
   
-  # Sidebar with a slider input for number of bins 
   sidebarLayout(
     sidebarPanel(
       textInput("age_p", label = h3("Positive Attitude & Age w/ different Lexicons"),value="1.1 attitude & age"),
@@ -28,7 +18,7 @@ shinyUI(fluidPage(
       textInput("bigram_n", label = h3("Tokenizing by Bigram"), value="4.2 from HATEs")
     ),
     
-    # Show a plot of the generated distribution
+
     mainPanel(
       tabsetPanel(
         tabPanel("Insight 1.1", plotOutput("age_p",  width = "100%", height = "800px")),
